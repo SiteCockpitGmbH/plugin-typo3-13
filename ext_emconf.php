e5a2e11597e62e598e3136eebdbb2bc07bcfadc1
Hau Tran <hau.tran@nfq.com>
2025-08-19 15:01:53 +0700

    SCNFQ-64 Add author informations

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,6 +3,11 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'easyVision by SiteCockpit',
     'description' => 'Integrate SiteCockpit into TYPO3: With SiteCockpit’s easyVision, your visitors can customize the website to their needs—for more accessibility and a better user experience. Integration takes just a few minutes!',
+    'category' => 'plugin',
+    'author' => 'Site Cockpit GmbH',
+    'author_email' => 'kontakt@sitecockpit.com',
+    'author_company' => 'Site Cockpit GmbH',
+    'state' => 'stable',
     'constraints' => [
         'depends' => [
             'typo3' => '13.0.0-13.4.99',
